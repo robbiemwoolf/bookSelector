a67f895b4956a2ea37e3a79efd3f670f638c6239
Robbie Woolf <robbiemwoolf@gmail.com>
2022-06-19 21:22:48 -0700

    functioning random book selector that uses an inputted books array

--- docs/index.js
+++ docs/index.js
@@ -1,25 +1,5 @@
 document  //document gives access to the DOM
 
-// const axios = require("axios");
-
-// google books API documentation https://developers.google.com/books/docs/v1/using#RetrievingBookshelf
-
-// const userId = "109437390009032914490";
-// const shelfId = "1001";
-// const BASE_URL = "https://www.googleapis.com/books/v1/users/";
-
-// want to access specific public bookshelf
-
-// function googleAPI() {
-//     axios
-//         .get(`${BASE_URL}${userId}/bookshelves/${shelfId}`)
-//         .then(response => {
-//             console.log(response)
-//         });
-// }
-
-// console.log(googleAPI())
-
 const books = [
     "The Vegetarian",
     "Strange Stars",
