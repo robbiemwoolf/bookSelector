e07d840b77a0a60da2e3a315d06e0a5e84ba13ac
Robbie Woolf <robbiemwoolf@gmail.com>
2022-06-19 21:31:14 -0700

    now have access to personal bookshelf with google api

--- docs/index.js
+++ docs/index.js
@@ -1,24 +1,26 @@
-document  //document gives access to the DOM
-
-// const axios = require("axios");
+const axios = require("axios");
 
 // google books API documentation https://developers.google.com/books/docs/v1/using#RetrievingBookshelf
 
-// const userId = "109437390009032914490";
-// const shelfId = "1001";
-// const BASE_URL = "https://www.googleapis.com/books/v1/users/";
+const userId = "109437390009032914490";
+const shelf = "1001";
+const BASE_URL = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelf}/volumes`
 
-// want to access specific public bookshelf
+// url no template literals: https://www.googleapis.com/books/v1/users/109437390009032914490/bookshelves/1001/volumes
 
-// function googleAPI() {
+// data object key "items" is an array of objects (each book with info). Inside each object we find the "volumeInfo" object that has a "title" key/value pair, a "subtitle" key/value pair, an "authors" key that holds an array of authors, and a "categories" key that holds an array of categories
+
+// focus on accessing the title first so that you can use your google books library to deliver the title of a random book rather than using a local array that you built
+
+// async function googleAPI() {
+//     const { data } = await axios.get(url)
+//     const { volumInfo: {title, subtitle, authors, categories}} = data
 //     axios
-//         .get(`${BASE_URL}${userId}/bookshelves/${shelfId}`)
+//         .get(url)
 //         .then(response => {
 //             console.log(response)
 //         });
 // }
-
-// console.log(googleAPI())
 
 const books = [
     "The Vegetarian",
@@ -34,8 +36,8 @@ const books = [
 // takes in an array, chooses a random index, and returns message
 function bookSelector(books) {
     let book = books[Math.floor(Math.random()*books.length)];
-    return `Read: ${book}`;;
-}
+    return `Read: ${book}`;
+};
 
 // Select the button
 //querySelector searches the DOM using a string representing a CSS selector
