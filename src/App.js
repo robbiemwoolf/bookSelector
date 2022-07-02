2f1dca681ebb1f62b56a6fb63f7897ea45e11001
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-01 19:50:52 -0700

    returns a random book title from a specific user's public bookshelf using google books api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
-import BookSelector from './BookSelector';
 import Books from './Books';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <BookSelector books={Books}/>
+        <Books />
       </header>
     </div>
   );
--- src/Books.js
+++ src/Books.js
@@ -1,27 +1,37 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import BookSelector from "./BookSelector";
+
+function Books() {
+
+    const userId = "109437390009032914490";
+    const shelf = "1001";
+    const url = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelf}/volumes`;
+    
+    const [apiData, setApiData] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setApiData(data));
+    }, []); // Passing [] so that it only runs the effect once
+    
+    let books = [];
+    if (apiData.length !== 0) {
+
+    const values = Object.values(apiData);
+
+    const items = values[2];
+
+    const volInfos = items.map(({ volumeInfo }) => volumeInfo) 
+
+    books = volInfos.map(book => book.title);
+    }
+    
+  return <BookSelector books={books} />
+}
+
+export default Books;
 
 // google books API documentation https://developers.google.com/books/docs/v1/using#RetrievingBookshelf
 
 // url no template literals: https://www.googleapis.com/books/v1/users/109437390009032914490/bookshelves/1001/volumes
-  
-// data object key "items" is an array of objects (each book with info). Inside each object we find the "volumeInfo" object that has a "title" key/value pair, a "subtitle" key/value pair, an "authors" key that holds an array of authors, and a "categories" key that holds an array of categories
-  
-// focus on accessing the title first so that you can use your google books library to deliver the title of a random book rather than using a local array that you built
-
-
-function Books() {
-  const userId = "109437390009032914490";
-  const shelf = "1001";
-  const url = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${shelf}/volumes`;
-   
-  const [data, setData] = useState({})
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then(setData);
-  }, []);
-  console.log(data)
-}
-
-export default Books;
